e791d1af0277958ca24f8f76ec6f3c2774b0c2fb
dirkengram1030 <dirkengram@hotmail.com>
2013-09-12 22:04:07 -0400

    developed formula for balancing budget

--- Expressions_Industry/js/Industry_Script.js
+++ Expressions_Industry/js/Industry_Script.js
@@ -1,2 +1,8 @@
 //Prompt and Alert
- var
+var budget = prompt ("Please enter your budget amount");
+var expenses = prompt ("please enter amount of donations");
+var jobs = prompt ("Please enter the number of jobs");
+var balance = budget * jobs - expenses;
+console.log("Your account balance is" + " $" + balance);
+alert(balance);
+
---
+++ Expressions_Wacky/js/Wacky.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: DirkEngram1
+ * Date: 9/12/13
+ * Time: 9:52 PM
+ * To change this template use File | Settings | File Templates.
+ */

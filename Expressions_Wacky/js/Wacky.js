a46bc6c01e4743bf3631f4d32368583a8e2f9573
dirkengram1030 <dirkengram@hotmail.com>
2013-09-12 23:19:04 -0400

    develop formula for comming up with time spent at gym each year

--- Expressions_Wacky/js/Wacky.js
+++ Expressions_Wacky/js/Wacky.js
@@ -9,6 +9,7 @@
 var TimeAtGym = ["Months" , "Days" , "Minutes"];
 var Months = prompt ("How many months out of the year do you go to the gym?");
 var Days = prompt ("How many days a week do you go to the gym? ");
-var Minutes = prompt ("How many minutes do you spend at the gym?")
+var Minutes = prompt ("How many minutes do you spend at the gym?");
+var Time = Months * Days * Minutes
 
 

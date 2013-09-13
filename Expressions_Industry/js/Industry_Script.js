35e0e471308d5875c24135f40bf7c2319e92d6df
dirkengram1030 <dirkengram@hotmail.com>
2013-09-12 23:31:04 -0400

    adding unit of measurment to the alert

--- Expressions_Industry/js/Industry_Script.js
+++ Expressions_Industry/js/Industry_Script.js
@@ -1,4 +1,5 @@
 Dirk Engram  September 12 2013 Expressions
+
 var budget = prompt ("Please enter your budget amount");
 var expenses = prompt ("please enter amount of donations");
 var jobs = prompt ("Please enter the number of jobs");

7226a40cfb7f3770e56075690c13b0ecfb769f3d
dirkengram1030 <dirkengram@hotmail.com>
2013-09-12 23:14:29 -0400

    developed variable for days spent at the gym.

--- Expressions_Wacky/js/Wacky.js
+++ Expressions_Wacky/js/Wacky.js
@@ -5,6 +5,10 @@
  * Time: 9:52 PM
  * To change this template use File | Settings | File Templates.
  */
+
 var TimeAtGym = ["Months" , "Days" , "Minutes"];
+var Months = prompt ("How many months out of the year do you go to the gym?");
+var Days = prompt ("How many days a week do you go to the gym? ");
+var Minutes = prompt ()
 
 

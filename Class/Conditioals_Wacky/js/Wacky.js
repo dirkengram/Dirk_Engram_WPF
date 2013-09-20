e056029e003b148fecc522822190e0afe0bb86e1
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 23:12:38 -0400

    developed variable for wether or not the user is old enough to drink alcohol

--- Class/Conditioals_Wacky/js/Wacky.js
+++ Class/Conditioals_Wacky/js/Wacky.js
@@ -9,5 +9,7 @@
 var curYear = prompt("Please Enter the current year", "2013");
 var yearBorn = prompt("Please Enter the year you were born", "1987");
 var age = curYear - yearBorn;
+var alcohol;
+
 
 

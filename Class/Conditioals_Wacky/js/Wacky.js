ff74ce14c2ad50300718037e0ec3f9a714bbf8d4
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 23:23:23 -0400

    name date assignment for wacky

--- Class/Conditioals_Wacky/js/Wacky.js
+++ Class/Conditioals_Wacky/js/Wacky.js
@@ -1,10 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: DirkEngram1
- * Date: 9/19/13
- * Time: 3:21 PM
- * To change this template use File | Settings | File Templates.
- */
+//Dirk A Engram September 19 2013 Conditional Assignment
 
 var curYear = prompt("Please Enter the current year", "2013");
 var yearBorn = prompt("Please Enter the year you were born", "1987");

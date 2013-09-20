4fad37b531f0c6bdb4f6b892f1bec21d8cf00621
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 22:54:31 -0400

    developed else if statement in case adult is old enough but the kid is not

--- Class/Conditionals_Industry/js/Industry.js
+++ Class/Conditionals_Industry/js/Industry.js
@@ -5,13 +5,15 @@
  * Time: 12:13 PM
  * To change this template use File | Settings | File Templates.
  */
-var age = prompt ("How old are you","21")
-var kidAge = prompt ("Please enter of child", "5")
+var age = prompt ("How old are you","21");
+var kidAge = prompt ("Please enter of child", "5");
 
 if(age >= 21 && kidAge < 6){
     console.log("You and your child may enter the movie and there is no charge for the child.")
 }else{ if(age < 21){
     console.log("You are not old enough to see this movie!")
+}else{ if(age >= 21 && kidAge > 5){
+    console.log("You and your child may enter the movie but the child is to old for a free ticket! ")
 
 }
 }

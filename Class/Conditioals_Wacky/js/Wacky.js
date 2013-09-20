665fe1c659f305f8f34d91c42a83cfbe64188334
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 23:05:44 -0400

    developed variable the the year the user enters

--- Class/Conditioals_Wacky/js/Wacky.js
+++ Class/Conditioals_Wacky/js/Wacky.js
@@ -7,11 +7,4 @@
  */
 
 var curYear = prompt("Please Enter the current year", "2013");
-var yearBorn = prompt("Please Enter the year you were born", "1987");
-var age = curYear - yearBorn;
-var alcohol;
-
-alcohol = (age > 21) ? "You are old enough to consume alcohol" : "You are not old enough to consume alcohol!";
-console.log(alcohol);
-alert(alcohol);
 

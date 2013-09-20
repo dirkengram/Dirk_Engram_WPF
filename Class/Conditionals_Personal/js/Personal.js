70246c3a8fbf123c06e596377a72caa55e53d2ee
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 23:22:31 -0400

    name date assignment for Personal

--- Class/Conditionals_Personal/js/Personal.js
+++ Class/Conditionals_Personal/js/Personal.js
@@ -1,10 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: DirkEngram1
- * Date: 9/19/13
- * Time: 1:41 PM
- * To change this template use File | Settings | File Templates.
- */
+//Dirk A Engram September 19 2013 Conditionals Assignment
 
 var dogs = prompt("Enter the number of dogs you own", "1");
 var weight = prompt ("Enter the combined weight of dogs/dog","55");

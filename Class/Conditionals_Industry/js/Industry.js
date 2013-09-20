6f8ee95a56c05dc93ce5d6541a0f025925da48b0
dirkengram1030 <dirkengram@hotmail.com>
2013-09-19 22:50:42 -0400

    developed formula for the adult and child entry to the movie with free child ticket

--- Class/Conditionals_Industry/js/Industry.js
+++ Class/Conditionals_Industry/js/Industry.js
@@ -8,6 +8,9 @@
 var age = prompt ("How old are you","21")
 var kidAge = prompt ("Please enter of child", "5")
 
+if(age >= 21 && kidAge < 6){
+    console.log("You and your child may enter the movie and there is no charge for the child.")
+
 
 }
 }

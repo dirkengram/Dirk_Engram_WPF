4bf4660454eec320967f50c9c465f382ed7e4142
dirkengram1030 <dirkengram@hotmail.com>
2013-09-23 20:22:47 -0400

    do while loops

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,4 +1,4 @@
-//Dirk A.Engram Loops
+//Dirk A.Engram  Do While Loops
 
 var b = 10; //setups the index
 
@@ -6,3 +6,13 @@ while ( b > 0){ //checks the condition
     console.log(b + ' kegs on the wall');
     b--;  //increments or decrements the index
 }
+
+console.log('------ Do While Loops------');
+
+var c = 10
+
+do{
+    console.log(c + ' kegs on the wall')
+    c--;
+}
+while(c > 0);

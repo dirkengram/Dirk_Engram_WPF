34e707bf9acbd732736e94e6ab2b90af93bcde96
dirkengram1030 <dirkengram@hotmail.com>
2013-09-26 23:25:13 -0400

    Commit

--- Expressions_Industry/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: DirkEngram1
- * Date: 9/12/13
- * Time: 1:05 PM
- * To change this template use File | Settings | File Templates.
- */
-
-prompt("Enter your year of birth");

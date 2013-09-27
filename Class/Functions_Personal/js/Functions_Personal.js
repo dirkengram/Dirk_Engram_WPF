28fd9808edb249b08ebc0d3b2973d024b997f047
dirkengram1030 <dirkengram@hotmail.com>
2013-09-26 23:09:02 -0400

    developed variable for the front side of the users house

--- Class/Functions_Personal/js/Functions_Personal.js
+++ Class/Functions_Personal/js/Functions_Personal.js
@@ -9,23 +9,4 @@
 
 confirm("First we must git the length in ft of each side of you house.");
 var sideOne = Number(prompt("What is the length in ft of the front side of your house?"));
-var sideTwo = Number(prompt("What is the length in ft of the left side of your house?"));
-var sideThree = Number(prompt("What is the length in ft of the right side of your house?"));
-var sideFour = Number(prompt("What is the length in ft of the back side of your house?"));
 
-//Perimeter = side1 + side2 + side3 + side4
-
-calcPerimeter = function(sideOne, sideTwo, sideThree, sideFour){
-    var perimeter = sideOne + sideTwo + sideThree + sideFour;
-    if(perimeter > 400){
-        console.log("Your house is " + perimeter + "ft. and bigger then my house.");
-    }else{
-        if(perimeter < 400){
-        console.log("Your house is " + perimeter + "ft. and smaller then my house.");
-        }
-     }
-    if(perimeter === 400){
-        console.log("Your house is 400ft. and the same size as my house.");
-    }
-};
-calcPerimeter(sideOne, sideTwo, sideThree, sideFour);

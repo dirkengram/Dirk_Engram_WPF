b844179978badd304cfdcf8a3713e65477defae5
dirkengram1030 <dirkengram@hotmail.com>
2013-09-26 23:07:26 -0400

    developed function to determine if user is eligible for a drivers license

--- Class/Functions_Industry/js/Functions_Industry.js
+++ Class/Functions_Industry/js/Functions_Industry.js
@@ -9,3 +9,14 @@
 confirm("This program will check your eligibility to obtain a Georgia drivers license.");
 var age = Number(prompt("How old are you?"));
 var requirement =prompt("Have you had you Learners Permit for one year?");
+var license;
+
+/*If you are under the age of 16 or has not meet the requirements then you are not eligible for a drivers
+  license in Georgia. */
+
+var eligible = function(){
+    license = ((age < 16) || (requirement === "no")) ? "You are not eligible for a drivers license!" : "You are eligible for a drivers license! Be safe.";
+    console.log(license);
+};
+
+
